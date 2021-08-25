119affb2a04ac131d3962b0bfbe08e5a5cf1c9bd
Eric Nantes <ericnantes18@gmail.com>
2021-08-25 18:54:13 -0400

    Listing with state

--- 01-myskills/src/pages/Home.js
+++ 01-myskills/src/pages/Home.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet, TextInput, Platform, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { 
+    SafeAreaView,
+    Text, 
+    StyleSheet, 
+    TextInput, 
+    Platform, 
+    TouchableOpacity 
+} from 'react-native';
 
 export function Home() {
-  return (
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>Welcome, Eric Nantes</Text>
+    const [newSkill, setNewSkill] = useState('');
+    const [mySkills, setMySkills] = useState([]);
 
-        <TextInput
-            style={styles.input}
-            placeholder="New skill"
-            placeholderTextColor="#555"
-        />
+    function handleAddNewSkill() {
+        setMySkills(oldState => [...oldState, newSkill]);
+        setNewSkill('');
+    }
+    
 
-        <TouchableOpacity style={styles.button} activeOpacity={.7}>
-            <Text style={styles.buttonText}>Add</Text>
-        </TouchableOpacity>
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Welcome, Eric Nantes
+            </Text>
 
-        <Text style={[styles.title, { marginTop: 20 }]}>
-            My Skills
-        </Text>
-      </SafeAreaView>
-  )
+            <TextInput
+                style={styles.input}
+                placeholder="New skill"
+                placeholderTextColor="#555"
+                onChangeText={setNewSkill}
+                value={newSkill}
+            />
+
+            <TouchableOpacity 
+                style={styles.button} 
+                activeOpacity={.7} 
+                onPress={handleAddNewSkill}
+            >
+                <Text style={styles.buttonText}>
+                    Add
+                </Text>
+            </TouchableOpacity>
+
+            <Text style={[styles.title, { marginTop: 20 }]}>
+                My Skills
+            </Text>
+
+            {mySkills.map(skill => (
+                <TouchableOpacity key={skill} style={styles.skillButton}>
+                    <Text style={styles.skillText}>{skill}</Text>
+                </TouchableOpacity>
+            ))}
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -54,5 +86,17 @@ const styles = StyleSheet.create({
         color: '#FFF',
         fontSize: 18,
         fontWeight: 'bold'
+    },
+    skillButton: {
+        backgroundColor: '#1F1E25',
+        padding: 15,
+        borderRadius: 10,
+        marginVertical: 10,
+        alignItems: 'center'
+    },
+    skillText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: "#FFF"
     }
 })
